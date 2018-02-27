c3b0f6aedf8f536cc313551eda632427e40a985b
Enes Demirag <enesdemirag1@hotmail.com>
2018-02-27 17:09:40 +0300

    Add log calss

---
+++ vehicle/modules/printer.py
@@ -0,0 +1,16 @@
+from datetime import datetime
+logpad = open("log.txt", "w") # May need full path here
+
+def error_decorator(error_func):
+    def wrapper(*arg):
+        print("ERROR: ", end="", file = logpad)
+        return error_func(*arg)
+    return wrapper
+
+@error_decorator
+def log(log_msg):
+    print(str(datetime.now()) + "\t" + log_msg, file = logpad)
+    
+@error_decorator
+def log_error(error_msg):
+    print(str(datetime.now()) + "\t" + error_msg, file = logpad)
