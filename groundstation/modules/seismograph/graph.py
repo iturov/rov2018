d69ae42682744c3ae4c745e1d7469dedeb9c29ea
Enes Demirag <enesdemirag1@hotmail.com>
2018-03-08 23:03:51 +0300

    Change file path

--- groundstation/modules/seismograph/graph.py
+++ groundstation/modules/seismograph/graph.py
@@ -1,14 +1,13 @@
 import matplotlib.pyplot as plt
-import matplotlib.animation as animation
+from matplotlib import animation
 from matplotlib import style
-
 style.use('seaborn-darkgrid')
 
 fig = plt.figure()
 axis1 = fig.add_subplot(1,1,1)
 
 def animate(i):
-    graph_data = open('/home/enes/Desktop/rov2018/groundstation/modules/seismograph/data.txt','r').read()
+    graph_data = open('data.txt','r').read()
     lines = graph_data.split('\n')
     xs = []
     ys = []
