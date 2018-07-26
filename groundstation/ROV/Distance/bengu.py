fff909a083a5b13981321f98c6852a103b924f90
Enes Demirag <enesdemirag1@hotmail.com>
2018-07-26 20:58:06 +0300

    Update distance measuring code

--- groundstation/ROV/Distance/bengu.py
+++ groundstation/ROV/Distance/bengu.py
@@ -4,21 +4,25 @@ import time
 import math
 import easygui
 
+constValues = []
 
-leftButton = win32con.VK_LBUTTON
-rightButton = win32con.VK_RBUTTON
+shiftButton = win32con.VK_SHIFT
+leftButton = win32con.VK_RIGHT
+rightButton = win32con.VK_LEFT
 downButton = win32con.VK_DOWN
 controlButton = win32con.VK_CONTROL
 esc = win32con.VK_ESCAPE
 
-def degerleriCek():
-    satirlar = open('values.txt', 'r')
-    geciciListe = []
-    for satir in satirlar:
-        geciciListe.append(satir)
-    knownLength = float(geciciListe[0])
-    focalLength = float(geciciListe[1])
-    return knownLength, focalLength
+def findFocal(length):
+    if (length < 10):
+        focal = 580
+    elif (length > 10 and length < 20):
+        focal = 630
+    elif (length > 20 and length < 30):
+        focal = 680
+    else:
+        focal = 730
+    return focal
 
 def MouseBaslangic():
     mousePosition = win32api.GetCursorPos()
@@ -32,12 +36,12 @@ def MouseTiklama():
     while True:
         if(win32api.GetAsyncKeyState(leftButton)):
             startPos = MouseBaslangic()
-            print("Start : ", startPos)
+            print("Start Point : ", startPos)
             time.sleep(0.2)
 
         if(win32api.GetAsyncKeyState(rightButton)):
             endPos = MouseSon()
-            print("Stop : ", endPos)
+            print("Stop Point : ", endPos)
             time.sleep(0.2)
 
         if(win32api.GetAsyncKeyState(downButton)):
@@ -46,24 +50,28 @@ def MouseTiklama():
             time.sleep(0.2)
             return distance
 
-def katSayiHesapla():
-    knownLength = int(easygui.enterbox(msg = 'Enter known length: '))
-    knownDistance = int(easygui.enterbox(msg = 'Enter known distance: '))
-    pixelLength = MouseTiklama()
-    hesaplananKatsayi = knownDistance * pixelLength / knownLength
-    degerler = [str(knownLength), '\n', str(hesaplananKatsayi)]
-    dosya = open('values.txt','w')
-    dosya.writelines(degerler)
-    dosya.close()
-
-
 while(1):
+    if(win32api.GetAsyncKeyState(shiftButton)):
+        cm = float(easygui.enterbox(msg='Enter known length: '))
+        focalConstant = findFocal(cm)
+        startVerticalp = MouseTiklama()
+        foundStartDistance = (focalConstant * cm) / startVerticalp
+        cm = float(easygui.enterbox(msg='Enter known length: '))
+        stopVerticalp = MouseTiklama()
+        foundStopDistance = (focalConstant * cm) / stopVerticalp
+        foundVerticalDistance = math.sqrt(foundStartDistance**2 - foundStopDistance**2)
+        easygui.msgbox(foundVerticalDistance, title="Result:")
+        file = open("resultVertical.txt","a") # Copy of Master ENES
+        file.write("\n" + str(foundVerticalDistance))
+        file.close()
     if(win32api.GetAsyncKeyState(controlButton)):
-        knownLength, focalLength = degerleriCek()
+        cm = float(easygui.enterbox(msg='Enter known length: '))
+        focalConstant = findFocal(cm)
         pixelLength = MouseTiklama()
-        foundDistance = (focalLength * knownLength) / pixelLength
+        foundDistance = (focalConstant * cm) / pixelLength
         easygui.msgbox(foundDistance, title="Result:")
+        file = open("result.txt", "a") # Don't fuc*ing delete this line
+        file.write("\n" + str(foundDistance)) # Don't fuc*ing delete this line
+        file.close() # Don't fuc*ing delete this line
     if (win32api.GetAsyncKeyState(esc)):
         break
-    if(win32api.GetAsyncKeyState(0x58)):
-        katSayiHesapla()
